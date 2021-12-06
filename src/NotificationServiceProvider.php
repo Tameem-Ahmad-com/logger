575ced4e491308ff6a83a3ca26f44af5f28a5034
Tameem Ahmad <tshahzad@computan.net>
2021-12-06 19:08:14 +0500

    migration table name changed

--- src/NotificationServiceProvider.php
+++ src/NotificationServiceProvider.php
@@ -58,11 +58,11 @@ class NotificationServiceProvider extends ServiceProvider
 
       //publishing config file
         $this->publishes([
-            __DIR__ . '/../config/config.php' => config_path('notification.php'),
+            __DIR__ . '/./config/config.php' => config_path('notification.php'),
         ], 'config');
       //publishing migrations
       $this->publishes([
-        __DIR__ . '/../database/migrations/create_debugging_logs_table.php.stub' =>
+        __DIR__ . '/./database/migrations/create_debugging_logs_table.php.stub' =>
          database_path('migrations/' . date('Y_m_d_His', time()) . '_create_debugging_logs_table.php'),
         // you can add any number of migrations here
       ], 'migrations');
