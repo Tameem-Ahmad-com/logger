c1038853f673b18f0f4de9304524369d6019fb75
Tameem Ahmad <tshahzad@computan.net>
2021-12-22 22:06:50 +0500

    page added with some basic design to show exceptions

--- src/resources/views/exceptions/list.blade.php
+++ src/resources/views/exceptions/list.blade.php
@@ -9,6 +9,8 @@ $code = app()->isDownForMaintenance() ? 'maintenance' : 'error logs';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $code }}</title>
     <link rel="stylesheet" href="//cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <style>
         body {
             margin: 2em;
@@ -66,6 +68,8 @@ $code = app()->isDownForMaintenance() ? 'maintenance' : 'error logs';
     <!--Scripts-->
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script src="//cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
     <script>
         $(document).ready(function() {
