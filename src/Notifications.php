0757917ed1141177b52b8f75c3748cc433530e56
Tameem Ahmad <tshahzad@computan.net>
2021-12-28 20:07:40 +0500

    new structure added

--- src/Notifications.php
+++ src/Notifications.php
@@ -495,17 +495,18 @@ class Notifications
     public static function getDevHtml($error)
     {
         $url = url('exceptions');
+        $appName = config('app.name');
         return <<<HTML
                     
                 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 
                 <table style="width: 800px;margin: 0 auto;background-color: #f9f9f9;padding: 16px;">
                 <tr style="background-color:#ff4153">
-                    <td><h1 style="margin-bottom:20px;text-align: center;margin: 0;font-size: 26px;padding: 20px; color: #fff;font-family: 'Roboto', sans-serif;font-weight: 700;">Error Occured on http://package.test</h1></td>
+                    <td><h1 style="margin-bottom:20px;text-align: center;margin: 0;font-size: 26px;padding: 20px; color: #fff;font-family: 'Roboto', sans-serif;font-weight: 700;">Exception occured</h1></td>
                 </tr>
                 <tr>
                     <td  style="padding-top: 20px;">
-                        <h3 style="margin:0;color:#000;font-family: 'Roboto', sans-serif;font-weight: 700;padding:0 20px;">Hi, Something went wrong on {config('app.name')}</h3>
+                        <h3 style="margin:0;color:#000;font-family: 'Roboto', sans-serif;font-weight: 700;padding:0 20px;">Hi, Something went wrong on {$appName}</h3>
                     </td>
                 </tr>
                 <tr>
@@ -522,7 +523,7 @@ class Notifications
                 </tr>
                 <tr>
                     <td>
-                        <p  style="    background-color: #2F4F4F;color: #fff;padding: 20px;margin:0 0 20px 0;line-height:1.8;font-size:16px;font-family: 'Roboto', sans-serif;font-weight: 400;">InvalidArgumentException: View [dda] not found. in D:\workspace\package\vendor\laravel\framework\src\Illuminate\View\FileViewFinder.php:137
+                        <p  style="    background-color: #2F4F4F;color: #fff;padding: 20px;margin:0 0 20px 0;line-height:1.8;font-size:16px;font-family: 'Roboto', sans-serif;font-weight: 400;">
                         <?php dump($error->context);?>
                       </p>
                     </td>
