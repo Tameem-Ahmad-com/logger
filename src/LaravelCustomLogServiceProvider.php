0c90e8b255ea81875aa6f6da7c4b55fc82dc6122
Tameem Ahmad <tshahzad@computan.net>
2021-12-28 19:38:40 +0500

    new structure added

--- src/LaravelCustomLogServiceProvider.php
+++ src/LaravelCustomLogServiceProvider.php
@@ -53,7 +53,7 @@ class LaravelCustomLogServiceProvider extends ServiceProvider
             $this->app->booted(function () {
                 $schedule = $this->app->make(Schedule::class);
                 if(!empty(config('custom-log.command'))){
-                    $schedule->job(new SendReportEmailJob())->{config('custom-log.command')};
+                    $schedule->job(new SendReportEmailJob())->cron(config('custom-log.command'));
                 }else{
                     $schedule->job(new SendReportEmailJob())->daily();
                 }
