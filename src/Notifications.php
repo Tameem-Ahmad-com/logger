39f61599cda98a35dc73e22704aec090b23a47eb
Tameem Ahmad <tshahzad@computan.net>
2021-12-29 18:19:47 +0500

    email design changed

--- src/Notifications.php
+++ src/Notifications.php
@@ -430,7 +430,7 @@ class Notifications
                                         <td bgcolor="#f4f4f4" align="center" style="padding: 30px 10px 0px 10px;">
                                             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
                                                 <tr>
-                                                    <td bgcolor="#9ca3af" align="center" style="padding: 30px 30px 30px 30px; border-radius: 4px 4px 4px 4px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                                                    <td bgcolor="#9ca3af" align="center" style="padding: 30px 30px 30px 30px; border-radius: 4px 4px 4px 4px; color: white; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
                                                         <h1 style="margin: 0;"><a href="#" target="_blank" style="color:white;">Total Exceptions:{$totalErrors}</a></h1>
                                                     </td>
                                                 </tr>
@@ -441,7 +441,7 @@ class Notifications
                                         <td bgcolor="#f4f4f4" align="center" style="padding: 30px 10px 0px 10px;">
                                             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
                                                 <tr>
-                                                    <td bgcolor="#9ca3af" align="center" style="padding: 30px 30px 30px 30px; border-radius: 4px 4px 4px 4px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                                                    <td bgcolor="#9ca3af" align="center" style="padding: 30px 30px 30px 30px; border-radius: 4px 4px 4px 4px; color: white; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
                                                         <h1 style="margin: 0;"><a href="#" target="_blank" style="color: white;">Total Jobs failed:{$jobsFailed}</a></h1>
                                                     </td>
                                                 </tr>
