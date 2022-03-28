d7096350e33ae7fed261f110626e3ccc77995474
Tameem Ahmad <tshahzad@computan.net>
2022-03-28 21:18:03 +0500

    report email layout changed

--- src/resources/views/emails/report.blade.php
+++ src/resources/views/emails/report.blade.php
@@ -2,10 +2,11 @@
 <html>
 
 <head>
-    <title></title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+        rel="stylesheet">
     <style type="text/css">
         @media screen {
             @font-face {
@@ -14,21 +15,18 @@
                 font-weight: 400;
                 src: local('Lato Regular'), local('Lato-Regular'), url(https://fonts.gstatic.com/s/lato/v11/qIIYRU-oROkIk8vfvxw6QvesZW2xOQ-xsNqO47m55DA.woff) format('woff');
             }
-
             @font-face {
                 font-family: 'Lato';
                 font-style: normal;
                 font-weight: 700;
                 src: local('Lato Bold'), local('Lato-Bold'), url(https://fonts.gstatic.com/s/lato/v11/qdgUG4U09HnJwhYI-uK18wLUuEpTyoUstqEm5AMlJo4.woff) format('woff');
             }
-
             @font-face {
                 font-family: 'Lato';
                 font-style: italic;
                 font-weight: 400;
                 src: local('Lato Italic'), local('Lato-Italic'), url(https://fonts.gstatic.com/s/lato/v11/RYyZNoeFgb0l7W3Vu1aSWOvvDin1pK8aKteLpeZ5c0A.woff) format('woff');
             }
-
             @font-face {
                 font-family: 'Lato';
                 font-style: italic;
@@ -36,24 +34,29 @@
                 src: local('Lato Bold Italic'), local('Lato-BoldItalic'), url(https://fonts.gstatic.com/s/lato/v11/HkF_qI1x_noxlxhrhMQYELO3LdcAZYWl9Si6vvxL-qU.woff) format('woff');
             }
         }
-
+        
+        * {
+            box-sizing: border-box;
+        }
+        
         table {
             width: 750px;
+            font-family: 'Open Sans', sans-serif;
             border-collapse: collapse;
             margin: 50px auto;
         }
-
         /* Zebra striping */
+        
         tr:nth-of-type(odd) {
             background: #eee;
         }
-
+        
         th {
             background: #3498db;
             color: white;
             font-weight: bold;
         }
-
+        
         td,
         th {
             padding: 10px;
@@ -61,93 +64,110 @@
             text-align: left;
             font-size: 18px;
         }
-
+        
+        td {
+            background: #fff;
+        }
         /*
 Max width before this PARTICULAR table gets nasty
 This query will take effect for any screen smaller than 760px
 and also iPads specifically.
 */
-        @media only screen and (max-width: 760px),
-        (min-device-width: 768px) and (max-device-width: 1024px) {
-
-            table {
-                width: 100%;
-            }
-
-            /* Force table to not be like tables anymore */
-            table,
-            thead,
-            tbody,
-            th,
-            td,
-            tr {
-                display: block;
-            }
-
-            /* Hide table headers (but not display: none;, for accessibility) */
-            thead tr {
-                position: absolute;
-                top: -9999px;
-                left: -9999px;
-            }
-
-            tr {
-                border: 1px solid #ccc;
-            }
-
-            td {
-                /* Behave  like a "row" */
-                border: none;
-                border-bottom: 1px solid #eee;
-                position: relative;
-                padding-left: 50%;
-            }
-
-            td:before {
-                /* Now like a table header */
-                position: absolute;
-                /* Top/left values mimic padding */
-                top: 6px;
-                left: 6px;
-                width: 45%;
-                padding-right: 10px;
-                white-space: nowrap;
-                /* Label the data */
-                content: attr(data-column);
-
-                color: #000;
-                font-weight: bold;
-            }
-
-        }
-
-
         /* ANDROID CENTER FIX */
+        
         div[style*="margin: 16px 0;"] {
             margin: 0 !important;
         }
-
+        
+        @media (max-width: 760px) {
+            table {
+                max-width: 95%;
+                width: 100%;
+                padding: 0px 20px;
+            }
+            td a {
+                word-break: break-all;
+            }
+            td h1 {
+                font-size: 25px !important;
+            }
+        }
     </style>
+    <script id="scripts" type="text/javascript" src="/scripts.js?15b1f7ebe8151ff8d70ca264310d0a23cd3e0d41"></script>
+    <link id="css-theme" rel="stylesheet" type="text/css" href="/css-theme.css?83b6d558d62b9116465ff862abd65097302c0980">
+    <meta name="referrer" content="same-origin" />
+
+    <link rel="icon" sizes="16x16" type="image/png" href="assets/favicon_16.png" />
+    <link rel="icon" sizes="32x32" type="image/png" href="assets/favicon_32.png" />
+    <link rel="icon" sizes="any" type="image/svg+xml" href="assets/favicon.svg" />
+    <title>Computan</title>
+    <meta name="application-name" content="Computan">
+    <meta name="apple-mobile-web-app-title" content="Computan">
+    <meta http-equiv="content-language" content="">
+    <meta name="language" content="">
+    <meta name="robots" content="NOINDEX,NOFOLLOW">
+    <meta name="msvalidate.01" content="">
+    <meta name="google-site-verification" content="">
+    <meta property="fb:app_id" content="">
+
+    <base href="/">
+
+    <script id="scripts" type="text/javascript" src="/scripts.js?15b1f7ebe8151ff8d70ca264310d0a23cd3e0d41"></script>
+    <link id="css-theme" rel="stylesheet" type="text/css" href="/css-theme.css?83b6d558d62b9116465ff862abd65097302c0980">
+    <meta name="referrer" content="same-origin" />
+
+    <link rel="icon" sizes="16x16" type="image/png" href="assets/favicon_16.png" />
+    <link rel="icon" sizes="32x32" type="image/png" href="assets/favicon_32.png" />
+    <link rel="icon" sizes="any" type="image/svg+xml" href="assets/favicon.svg" />
+    <title>Computan</title>
+    <meta name="application-name" content="Computan">
+    <meta name="apple-mobile-web-app-title" content="Computan">
+    <meta http-equiv="content-language" content="">
+    <meta name="language" content="">
+    <meta name="robots" content="NOINDEX,NOFOLLOW">
+    <meta name="msvalidate.01" content="">
+    <meta name="google-site-verification" content="">
+    <meta property="fb:app_id" content="">
+
+    <base href="/">
+
+    <script id="scripts" type="text/javascript" src="/scripts.js?15b1f7ebe8151ff8d70ca264310d0a23cd3e0d41"></script>
+    <link id="css-theme" rel="stylesheet" type="text/css" href="/css-theme.css?83b6d558d62b9116465ff862abd65097302c0980">
+    <meta name="referrer" content="same-origin" />
+
+    <link rel="icon" sizes="16x16" type="image/png" href="assets/favicon_16.png" />
+    <link rel="icon" sizes="32x32" type="image/png" href="assets/favicon_32.png" />
+    <link rel="icon" sizes="any" type="image/svg+xml" href="assets/favicon.svg" />
+    <title>Computan</title>
+    <meta name="application-name" content="Computan">
+    <meta name="apple-mobile-web-app-title" content="Computan">
+    <meta http-equiv="content-language" content="">
+    <meta name="language" content="">
+    <meta name="robots" content="NOINDEX,NOFOLLOW">
+    <meta name="msvalidate.01" content="">
+    <meta name="google-site-verification" content="">
+    <meta property="fb:app_id" content="">
+
+    <base href="/">
+
 </head>
 
 <body style="background-color: #e5e7eb; margin: 0 !important; padding: 0 !important;">
     <!-- HIDDEN PREHEADER TEXT -->
-    <div
-        style="display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: 'Lato', Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;">
+    <div style="display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: 'Lato', Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;">
         This email is provide you reporting about integration projects status.
     </div>
     <table border="0" cellpadding="0" cellspacing="0" width="100%" bgcolor="white">
-        <table bgcolor="white">
-            <table bgcolor="white">
+        <table style="background-color: #fff;">
+            <tbody>
                 <tr>
-                    <td style="text-align:center">
-                        <h1 style="align-content:center">{{ ucfirst(config('app.name')) }}</h1>
+                    <td style="text-align:center; background-color: #fff;">
+                        <h1 style=" font-weight: 700; font-size: 36px; align-content:center">{{ ucfirst(config('app.name')) }}</h1>
                     </td>
                 </tr>
-            </table>
             </tbody>
         </table>
-        <table bgcolor="white">
+        <table background-color="white">
             <thead>
                 <tr>
                     <th>No</th>
@@ -155,30 +175,27 @@ and also iPads specifically.
                 </tr>
             </thead>
             <tbody>
-                @php
-                    $i = 1;
+                @php $i = 1;
                 @endphp
                 @foreach ($exceptions as $exception)
-                    <tr>
-                        <td data-column="No">{{ $i++ }}</td>
-                        <td data-column="Exception">
-                            <a href="{{ url('exceptions/' . $exception->id . '/show') }}"
-                                title="please click on link to view the details" style="text-decoration: none;">
+                <tr>
+                    <td data-column="No">{{ $i++ }}</td>
+                    <td data-column="Exception">
+                        <a href="{{ url('exceptions/' . $exception->id . '/show') }}" title="please click on link to view the details" style="text-decoration: none;">
                                 {{ $exception->message }}</a>
-                        </td>
+                    </td>
 
-                    </tr>
+                </tr>
                 @endforeach
+
             </tbody>
         </table>
         <table style="margin-bottom:15px;">
-            <th style="text-align:center">
-                <a href="{{ url('exceptions?pass=' . \Crypt::encryptString('info@hellokongo.com')) }}" target="_blank"
-                    style="text-align:center;color:white">
+            <th style="text-align:center; border: none;">
+                <a href="{{ url('exceptions?pass=' . \Crypt::encryptString('info@hellokongo.com')) }}" target="_blank" style=" font-weight: 700; font-size: 18px; text-align:center;color:white">
                     View All</a>
             </th>
         </table>
-    </table>
     </table>
 
 
