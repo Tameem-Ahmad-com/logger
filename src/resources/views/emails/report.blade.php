6f0c184df1858e2e9661a13c86af478bb9ee70cf
tameem <64251041+tameemahmad@users.noreply.github.com>
2022-04-25 18:05:53 +0500

    report outlook 2016 responsiveness added

--- src/resources/views/emails/report.blade.php
+++ src/resources/views/emails/report.blade.php
@@ -1,11 +1,26 @@
-<!DOCTYPE html>
+<!--
+templateType: email
+isAvailableForNewContent: true
+-->
+<!doctype html>
 <html>
 
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+    <title>
+        {% if content.html_title and content.html_title != '' %}{{ content . html_title }}{% else %}{{ content . body . subject }}{% endif %}
+    </title>
+    {% if content.meta_description %}
+    <meta name="description" content="{{ content . meta_description }}" />{% endif %}
+    <style type="text/css" id="hs-inline-css">
+        /*<![CDATA[*/
+        /* everything in this style tag will be inlined onto matching elements */
+        .sample-rule {}
+
+        /*]]>*/
+
+    </style>
+    <link
+        href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
         rel="stylesheet">
     <style type="text/css">
         @media screen {
@@ -15,18 +30,21 @@
                 font-weight: 400;
                 src: local('Lato Regular'), local('Lato-Regular'), url(https://fonts.gstatic.com/s/lato/v11/qIIYRU-oROkIk8vfvxw6QvesZW2xOQ-xsNqO47m55DA.woff) format('woff');
             }
+
             @font-face {
                 font-family: 'Lato';
                 font-style: normal;
                 font-weight: 700;
                 src: local('Lato Bold'), local('Lato-Bold'), url(https://fonts.gstatic.com/s/lato/v11/qdgUG4U09HnJwhYI-uK18wLUuEpTyoUstqEm5AMlJo4.woff) format('woff');
             }
+
             @font-face {
                 font-family: 'Lato';
                 font-style: italic;
                 font-weight: 400;
                 src: local('Lato Italic'), local('Lato-Italic'), url(https://fonts.gstatic.com/s/lato/v11/RYyZNoeFgb0l7W3Vu1aSWOvvDin1pK8aKteLpeZ5c0A.woff) format('woff');
             }
+
             @font-face {
                 font-family: 'Lato';
                 font-style: italic;
@@ -34,67 +52,73 @@
                 src: local('Lato Bold Italic'), local('Lato-BoldItalic'), url(https://fonts.gstatic.com/s/lato/v11/HkF_qI1x_noxlxhrhMQYELO3LdcAZYWl9Si6vvxL-qU.woff) format('woff');
             }
         }
-        
+
         * {
             box-sizing: border-box;
         }
-        
+
         table {
             width: 750px;
             font-family: 'Open Sans', sans-serif;
             border-collapse: collapse;
-            margin: 50px auto;
+            margin: 0px auto;
         }
+
         /* Zebra striping */
-        
+
         tr:nth-of-type(odd) {
             background: #eee;
         }
-        
+
         th {
             background: #3498db;
             color: white;
             font-weight: bold;
         }
-        
+
         td,
         th {
             padding: 10px;
-            border: 1px solid #ccc;
+            /*      border: 1px solid #ccc; */
             text-align: left;
             font-size: 18px;
         }
-        
+
         td {
             background: #fff;
         }
+
         /*
-Max width before this PARTICULAR table gets nasty
-This query will take effect for any screen smaller than 760px
-and also iPads specifically.
-*/
+      Max width before this PARTICULAR table gets nasty
+      This query will take effect for any screen smaller than 760px
+      and also iPads specifically.
+      */
         /* ANDROID CENTER FIX */
-        
+
         div[style*="margin: 16px 0;"] {
             margin: 0 !important;
         }
-        
+
         @media (max-width: 760px) {
             table {
                 max-width: 95%;
                 width: 100%;
                 padding: 0px 20px;
             }
+
             td a {
                 word-break: break-all;
             }
+
             td h1 {
                 font-size: 25px !important;
             }
         }
+
     </style>
     <script id="scripts" type="text/javascript" src="/scripts.js?15b1f7ebe8151ff8d70ca264310d0a23cd3e0d41"></script>
-    <link id="css-theme" rel="stylesheet" type="text/css" href="/css-theme.css?83b6d558d62b9116465ff862abd65097302c0980">
+    <link id="css-theme" rel="stylesheet" type="text/css"
+        href="/css-theme.css?83b6d558d62b9116465ff862abd65097302c0980">
     <meta name="referrer" content="same-origin" />
 
     <link rel="icon" sizes="16x16" type="image/png" href="assets/favicon_16.png" />
@@ -113,7 +137,8 @@ and also iPads specifically.
     <base href="/">
 
     <script id="scripts" type="text/javascript" src="/scripts.js?15b1f7ebe8151ff8d70ca264310d0a23cd3e0d41"></script>
-    <link id="css-theme" rel="stylesheet" type="text/css" href="/css-theme.css?83b6d558d62b9116465ff862abd65097302c0980">
+    <link id="css-theme" rel="stylesheet" type="text/css"
+        href="/css-theme.css?83b6d558d62b9116465ff862abd65097302c0980">
     <meta name="referrer" content="same-origin" />
 
     <link rel="icon" sizes="16x16" type="image/png" href="assets/favicon_16.png" />
@@ -132,7 +157,8 @@ and also iPads specifically.
     <base href="/">
 
     <script id="scripts" type="text/javascript" src="/scripts.js?15b1f7ebe8151ff8d70ca264310d0a23cd3e0d41"></script>
-    <link id="css-theme" rel="stylesheet" type="text/css" href="/css-theme.css?83b6d558d62b9116465ff862abd65097302c0980">
+    <link id="css-theme" rel="stylesheet" type="text/css"
+        href="/css-theme.css?83b6d558d62b9116465ff862abd65097302c0980">
     <meta name="referrer" content="same-origin" />
 
     <link rel="icon" sizes="16x16" type="image/png" href="assets/favicon_16.png" />
@@ -149,56 +175,93 @@ and also iPads specifically.
     <meta property="fb:app_id" content="">
 
     <base href="/">
-
 </head>
 
-<body style="background-color: #e5e7eb; margin: 0 !important; padding: 0 !important;">
-    <!-- HIDDEN PREHEADER TEXT -->
-    <div style="display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: 'Lato', Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;">
+<body>
+    <!-- Preview text (text which appears right after subject in certain email clients) -->
+    <div id="preview_text" style="display:none!important">
+        {% text "preview_text" label="Preview Text <span class=help-text>This will be used as the preview text that displays in some email clients</span>", value="", no_wrapper=True %}
+    </div>
+    <div
+        style="display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: 'Lato', Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;">
         This email is provide you reporting about integration projects status.
     </div>
     <table border="0" cellpadding="0" cellspacing="0" width="100%" bgcolor="white">
-        <table style="background-color: #fff;">
-            <tbody>
-                <tr>
-                    <td style="text-align:center; background-color: #fff;">
-                        <h1 style=" font-weight: 700; font-size: 36px; align-content:center">{{ ucfirst(config('app.name')) }}</h1>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-        <table background-color="white">
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Exception</th>
-                </tr>
-            </thead>
-            <tbody>
-                @php $i = 1;
-                @endphp
-                @foreach ($exceptions as $exception)
-                <tr>
-                    <td data-column="No">{{ $i++ }}</td>
-                    <td data-column="Exception">
-                        <a href="{{ url('exceptions/' . $exception->id . '/show') }}" title="please click on link to view the details" style="text-decoration: none;">
-                                {{ $exception->message }}</a>
-                    </td>
-
-                </tr>
-                @endforeach
-
-            </tbody>
-        </table>
-        <table style="margin-bottom:15px;">
-            <th style="text-align:center; border: none;">
-                <a href="{{ url('exceptions?pass=' . \Crypt::encryptString('info@hellokongo.com')) }}" target="_blank" style=" font-weight: 700; font-size: 18px; text-align:center;color:white">
-                    View All</a>
-            </th>
-        </table>
+        <tbody>
+            <tr>
+                <td>
+                    <table border="0" cellpadding="0" cellspacing="0" width="100%" height="100%"
+                        style="background-color: #fff;">
+                        <tbody>
+                            <tr>
+                                <td style="height: 50px; border: 0;" height="50"></td>
+                            </tr>
+                            <tr>
+                                <td style="height: 117px; text-align:center; background-color: #fff; border: 1px solid #ccc;"
+                                    height="117">
+                                    <h1 style=" font-weight: 700; font-size: 36px; align-content:center">
+                                        {{ ucfirst(config('app.name')) }}</h1>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="height: 50px; border: 0;" height="50"></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <table border="0" cellpadding="0" cellspacing="0" width="100%" height="100%"
+                        background-color="white">
+                        <thead>
+                            <tr>
+                                <th style="border: 1px solid #ccc;">No</th>
+                                <th style="border: 1px solid #ccc;">Exception</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @php$i = 1;
+                            @endphp
+                            @foreach ($exceptions as $exception)
+                                <tr>
+                                    <td data-column="No" style="border: 1px solid #ccc;">{{ $i++ }}</td>
+                                    <td data-column="Exception" style="border: 1px solid #ccc;">
+                                        <a href="{{ url('exceptions/' . $exception->id . '/show') }}"
+                                            title="please click on link to view the details"
+                                            style="text-decoration: none; color: #3498db;">
+                                            {{ $exception->message }}
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                         </tbody>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <table border="0" width="100%" height="100%" cellpadding="0" cellspacing="0">
+                        <tbody>
+                            <tr>
+                                <td style="text-align:center; border: none; background: #3498db;">
+                                    <a href="{{ url('exceptions?pass=' . \Crypt::encryptString('info@hellokongo.com')) }}"
+                                        target="_blank"
+                                        style=" font-weight: 700; font-size: 18px; text-align:center;color:white">
+                                        View All</a>
+                                </td>
+                            </tr>
+                         </tbody>
+                    </table>
+                </td>
+            </tr>
+        </tbody>
     </table>
 
 
+
+
+    <!-- Office location information and unsubscribe links -->
 
 </body>
 
